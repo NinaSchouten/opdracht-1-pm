a8d0963d1e5abdaf704434d6e55559492b444263
NinaSchouten <s3909409@vuw.leidenuniv.nl>
2023-09-11 20:14:09 +0200

    infoblok gemaakt

---
+++ inlever1.cpp
@@ -0,0 +1,27 @@
+#include <iostream> // altijd erbij
+
+using namespace std;
+
+// const double FoutMarge = 0.1;
+
+int main ( ) {
+    // infoblok (misschien nog beter maken)
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "De makers zijn: " << endl;
+    cout << "Nina Schouten: 2005, Wiskunde + Inforrmatica, 3909409" << endl;
+    cout << "Simon (ACHTERNAAM): (JAARTAL), Informatica, (STUDENTNUMMER)" << endl;
+    cout << "Opdracht 1" << endl;
+    cout << "Gemaakt op 11-09-2023" << endl;
+    cout << "Op de volgende schermen zullen vragen staan die door u beantwoord moeten worden." << endl;
+    cout << "Hierdoor kunnen we een uitslag eruit krijgen die u zal vertellen of u wel," << endl;
+    cout << "of niet bent toegelaten tot de door u gekozen studie." << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+
+    // int gebruikerjaar gebruikermaand gebruiker dag
+
+    // gebruikerjaar < 1900 // gebruikerjaar > 2013 met if else statement
+
+    // 
+    
+
+}  // main
